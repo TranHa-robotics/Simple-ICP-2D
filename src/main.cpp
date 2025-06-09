c9fc748e346c1a48c339a733ab274adc188c8bb7
anhHa <hoangha5657@gmail.com>
2025-06-09 13:34:25 +0700

    add find nearest neighbor step

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,21 @@
 // using namespace std;
 using namespace Eigen;
 
+void findNearestNeighbors(const std::vector<Vector2d>& source, const std::vector<Vector2d>& dest, std::vector<int>& indices) {
+  indices.clear();
+  for(const auto&p : source) {
+    double min_distance = 1e9;
+    int idx = 0;
+    for(int i = 0; i<dest.size(); i++) {
+      double distance = (p - dest[i]).squaredNorm();
+        if(distance < min_distance) {
+          idx = i;
+          min_distance = distance;
+        }
+    }
+    indices.push_back(idx);
+  }
+}
 void applyTransformation(const Matrix2d &rot, const Vector2d &transl, std::vector<Vector2d> &dst) {
   for(auto& p: dst) {
     p = rot*p + transl;
@@ -28,5 +43,12 @@ int main() {
   for(auto p: transformed){
     std::cout << std::fixed << std::setprecision(4) << p[0] << " " << p[1] << std::endl;  
   }
+
+  // for(int i = 0; i<20; i++) {
+    std::vector<int> indices;
+    findNearestNeighbors(origin, transformed, indices);
+    for(auto i : indices)
+      std::cout << i << " " << std::endl;
+  // }
   return 0;
 }
